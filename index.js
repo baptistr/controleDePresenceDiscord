9d5b9fe82ad5db4095092e7f86da42f487b239dd
baptistr <baptiste.lemonnier@gmail.com>
2022-03-16 17:35:33 +0100

    add : an event list can start automatically

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const MongoClient = require('mongodb').MongoClient;
 const {generatePdf} = require("./generate-pdf.js");
 const {convertVCS} = require("./convert-vsc-to-json/convert-vcs-to-json.js");
 const { memberNicknameMention } = require('@discordjs/builders');
+const cron = require("node-cron");
+
 
 const client = new Client({ 
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -219,6 +221,95 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
+//tâche répétitive (toute les 30 secondes)
+//elle permet de voir si un évènement futur va bientot commencé
+cron.schedule('30,59 * * * * *', () => {
+
+    //Si il y a un évènement en cours, je ne vais pas plus loin
+    let request = fs.readFileSync('inProgress.json');
+    let res = JSON.parse(request);
+    if(res[0] == 1){
+        return;
+    }
+
+    //j'examine le edt.json
+    let edt = fs.readFileSync('edt.json');
+    edt = JSON.parse(edt);
+
+    //je vérifie si l'heure de maintenant correpond à une date d'un évènement dans l'edt.json
+    let dateNow = Math.floor(Date.now() / 1000);
+    let heureDebut = null;
+    let heureFin = null; 
+    let event = false;
+    edt.forEach(element => {
+        if(dateNow > element[1] && dateNow < element[2]){
+            //si oui, je récupère le l'heure du début et de fin de l'évènement
+            client.channels.cache.get(channel).send(":warning: :warning: Un évènement va bientôt démarrer ! :warning: :warning:");
+            heureDebut = element[1];
+            heureFin = element[2];
+            //une fois sauvegardé, je peux le supprimer
+            let myIndex = edt.indexOf(element);
+            edt.splice(myIndex, 1);
+
+            event = true;
+        }
+    });
+
+    //ne pas aller plus loin si aucun event ne possède le bon timestamp
+    if(!event){
+        return;
+    }
+
+    console.log("Un évènement est en cours");
+
+    //je renvoie le json avec la modification
+    edt = JSON.stringify(edt);
+    fs.writeFileSync('edt.json', edt);
+
+    //je modifie les données de l'évènement en cours dans inProgress
+    res[0] = 1;
+    res[1] = "useless";
+    res[2] = "useless";
+    res[3] = "useless";
+    res[4] = heureFin-heureDebut;
+    res[5] = heureFin;
+    res[6] = [];
+    let newarray = JSON.stringify(res);
+    fs.writeFileSync('inProgress.json', newarray);
+
+    client.channels.cache.get(channel).send(":white_check_mark: :white_check_mark: L'évènement a démarré avec succès, vous pouvez maintenant signer ! :white_check_mark: :white_check_mark:");
+
+    //j'attends que l'évènement soit terminé pour envoyer les données sur mongodb et je réinitialise le fichier inProgress
+    setTimeout(() => {
+
+        let request = fs.readFileSync('inProgress.json');
+        let res = JSON.parse(request);
+
+        client.channels.cache.get(channel).send(":warning: :warning: L'évènement est terminé, tu peux maintenant en initialiser un nouveau ! :warning: :warning:");
+        let newEvent = {
+            duration:res[4],
+            endEvent:res[5],
+            pdfPath:'pdf/appel-'+res[5]+'.pdf',
+            student:res[6]
+        }
+        
+        MongoClient.connect(mongoPath, function(err, db) {
+            if (err) throw err;
+            let dbo = db.db("present");
+            dbo.collection("present").insertOne(newEvent, function(err, res) {
+                if (err) throw err;
+                db.close();
+            });
+        });
+
+        res = [0];
+
+        let newarray = JSON.stringify(res);
+        fs.writeFileSync('inProgress.json', newarray);
+        
+    }, res[4]*1000);
+});
+
 const onReady = (message) => {
 
     if (!fs.existsSync("eleves.json")) {
@@ -234,7 +325,7 @@ const onReady = (message) => {
         fs.writeFileSync('edt.json', JSON.stringify([]));
     }
 
-    convertVCS('GPU_semaine_10_FIL');
+    //convertVCS('GPU_semaine_10_FIL');
 
     console.log("Je suis prêt à vous écouter !");
 };
